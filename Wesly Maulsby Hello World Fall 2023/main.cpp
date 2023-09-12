36f2b35f4f54ae0691749e8b4b6b1e6f00d931dd
WeslyM003 <maulsbywes@gmail.com>
2023-09-12 08:10:48 -0500

    Add project files.

---
+++ Wesly Maulsby Hello World Fall 2023/main.cpp
@@ -0,0 +1,19 @@
+/*
+	Wesly
+	C++ Fall 2023
+	Due: Fri Sep 15, 2023 11:59pm
+	Lab 1 "Hello World"
+	Output Hello World to the console
+*/
+
+//includes library needed for input and output
+#include <iostream>
+
+int main()
+{
+	//outputs the text "Hello World" to the command console
+	std::cout << "Hello World";
+
+	//returns function as 0 to show no errors have occured.
+	return 0;
+}
